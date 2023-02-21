0a0895a8dd7fc4be43a08b72e9d2f61ad635aa15
helmihidzir <helmihidzir@gmail.com>
2023-02-21 14:58:47 +0800

    complete

--- src/controllers/wordle_controller.js
+++ src/controllers/wordle_controller.js
@@ -1,25 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = [ "letters", "letter", "guessInput", "feedbackList" ]
+    static targets = [ "start" ]
 
     initialize() {
         this.generateWord()
+        this.startTarget.focus()
+        this.guesses = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ]
+        this.row = 1
     }
 
     generateWord() {
         // Generate a random 5-letter word
-        const words = ["apple", "banana", "cherry", "orange", "grape", "lemon"]
+        const words = ["apple", "grape", "lemon"]
         this.word = words[Math.floor(Math.random() * words.length)]
+        console.log(this.word)
         this.wordLetters = this.word.split("")
-        this.guesses = []
-        this.renderWord()
     }
 
-    handleEvent(event) {
+    handleInput(event) {
         const key = event.key
         if (/^[a-zA-Z]$/.test(key)) {
-            console.log(key)
+            event.target.classList.add("visited")
+            this.guesses[this.row - 1].push(key)
+
             const nextInput = event.target.nextElementSibling
             if (nextInput) {
                 nextInput.focus()
@@ -31,18 +42,66 @@ export default class extends Controller {
                 prevInput.value = ""
             }
         } else if(event.key === "Enter") {
-            alert("Check for answer")
+            this.guess()
         }
     }
 
-    renderWord() {
+    flipTile() {
+        const tileRowChildren = document.getElementById(`row-${this.row}`).children
 
+        this.guesses[this.row - 1].forEach((guess, index) => {
+            setTimeout(() => {
+                let overlayColor = 'grey-overlay'
+
+                if(this.wordLetters.includes(guess)) {
+                    overlayColor = 'yellow-overlay'
+                }
+
+                if(guess === this.wordLetters[index]) {
+                    overlayColor = 'green-overlay'
+                }
+
+                tileRowChildren[index].classList.add('flip')
+                tileRowChildren[index].classList.add(overlayColor)
+            }, 500 * index)
+        })
+
+        // won
+        if(this.guesses[this.row - 1].join("") === this.word) {
+            setTimeout(() => {
+                alert("Correct")
+                this.reset()
+            }, 3000)
+            return
+        }
+
+        // game ends
+        if(this.row === 6) {
+            setTimeout(() => {
+                alert(this.word)
+                this.reset()
+            }, 3000)
+            return
+        }
     }
 
     guess() {
+        if(this.guesses[this.row - 1].length < 5 ) {
+            alert("Not enough letters")
+            return
+        }
 
+        this.flipTile()
+
+        // next round
+        this.row += 1
+
+        if(document.getElementById(`row-${this.row}`)) {
+            document.getElementById(`row-${this.row}`).children[0].focus()
+        }
     }
 
     reset() {
+        location.reload()
     }
 }
